7bf080fb48fac7d620d1ec6e8bc56d1e0d16900f
Emily Dokken <dokkenemily@gmail.com>
2022-02-06 15:49:28 -0800

    create GET route for all Products with category and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id',
+    ],
+      // include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        attributes: [
+          'id',
+          'category_name'
+        ]
+      },
+      {
+        model: Tag,
+        attributes: [
+          'id',
+          'tag_name'
+        ]
+      }
+    ]
+  })
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
