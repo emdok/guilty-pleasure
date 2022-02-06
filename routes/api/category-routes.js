4c5d0028f1e71bea188eb8f3ee517a6f50756835
Emily Dokken <dokkenemily@gmail.com>
2022-02-06 15:22:22 -0800

    add GET route to get single category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
   // find all categories
   Category.findAll({
@@ -34,8 +33,42 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+      // include its associated Product
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  })
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+  // be sure to include its associated Products
+
 
 router.post('/', (req, res) => {
   // create a new category
