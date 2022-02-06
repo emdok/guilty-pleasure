3270f4b3f14a49c11c6de6533be0d78b0b85ccce
Emily Dokken <dokkenemily@gmail.com>
2022-02-06 11:51:37 -0800

    create associations between tables within the models/index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category Product belongs to Category, as a category can have multiple products but a product can only belong to one category.
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
 module.exports = {
   Product,
