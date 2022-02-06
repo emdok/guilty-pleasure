5910ae1ae7246680645d68e2dd05516f3eb78740
Emily Dokken <dokkenemily@gmail.com>
2022-02-06 15:26:38 -0800

    build POST route to create new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -67,11 +67,17 @@ router.get('/:id', (req, res) => {
     res.status(500).json(err);
   });
 });
-  // be sure to include its associated Products
-
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })    
+  .then((dbCategoryData) => res.json(dbCategoryData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
