7ea8f645f889c793fe34d102581f248a5f7a6110
Emily Dokken <dokkenemily@gmail.com>
2022-02-06 17:09:34 -0800

    clean up code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+
 router.get('/', (req, res) => {
   // find all categories
   Category.findAll({
