aa66c9e78c20d65281b300047c4d60a4470d24db
Emily Dokken <dokkenemily@gmail.com>
2022-02-06 16:03:43 -0800

    create route to locate a single product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
-// get all products
 router.get('/', (req, res) => {
   // find all products
   Product.findAll({
@@ -42,7 +40,47 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne(
+    {
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'product_name',
+        'price',
+        'stock',
+        'category_id',
+      ],
+        //include its associated Category and Tag data
+      include: [
+        {
+          model: Category,
+          attributes: [
+            'id',
+            'category_name'
+          ]
+        },
+        {
+          model: Tag,
+          attributes: [
+            'id',
+            'tag_name'
+          ]
+        }
+      ]
+  })
+  .then((dbProductData) => {
+    if (!dbProductData) {
+      res.status(404).json({ message: "No product found with this id" });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
