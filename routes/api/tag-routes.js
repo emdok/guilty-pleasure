bc91b3adde2731485c9ec7157547e4aeb2cf26e9
Emily Dokken <dokkenemily@gmail.com>
2022-02-06 16:51:16 -0800

    build route to create new tag name

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -70,7 +70,27 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create(
+    {
+    tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then((dbTagData) => {
+    if (!dbTagData) {
+      res.status(404).json({ message: "No tag found with this id "});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
